3ce150c39ac091b2ba952289cded3caa8602af97
Dabrin <dabrinltecr@gmail.com>
2021-01-05 23:14:59 -0600

    Proyecto Base
    Esto es el proyecto base con el que comenzamos en proj

---
+++ index.php
@@ -0,0 +1,35 @@
+<?php
+    require 'libs/FrontController.php';
+    FrontController::main();
+?>
+
+<!--<html>
+    
+    <body>
+        <form action="accion.php" method="post">
+            <div>
+                <label for="nombre">Nombre:</label>
+                <input type="text" id="nombre" name="nombre"/>                  
+            </div>
+            <div>
+                <label for="apellidos">Apellidos:</label>
+                <input type="text" id="apellidos" name="apellidos"/>                  
+            </div>
+            <div>
+                <input type="submit" value="Enviar"/>                  
+            </div>            
+        </form>
+    </body>
+    
+</html>-->
+
+
+
+<?php
+    
+//    include_once 'Persona.php';
+//    echo 'Hola<br>';
+//    $persona=new Persona("Nelson", "Mendez");
+//    echo 'Persona '.$persona;
+?>
+
---
+++ public/header.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta charset="utf-8"/>
+        <title>Sitio ejemplo curso Lenguajes</title>
+        <meta name="description" content="Esto es un ejemplo de un sitio web para el curso de Lenguajes"/>
+        <meta name="viewport" content="width=device-width,initial-scale=1"/>
+        <link rel="shortcut icon" type="image/x-icon" href="img/icono.ico"/>
+        <link rel="stylesheet" type="text/css" href="public/css/estilo.css"/>
+    </head>
+    <body>
+        <header>
+            <h1>
+                <a href="index.html">
+                    <img alt="Curso Lenguajes" src="public/img/logo.png"/>
+                </a>
+            </h1>
+            <nav>
+                <ul>
+                    <li><a href="index.html">Inicio</a></li>
+                    <li><a href="formulario.html">Formulario</a> </li>
+                </ul>
+            </nav>
+        </header>
+        
+        <section id="contenido">
+            <section id="principal">
+               
